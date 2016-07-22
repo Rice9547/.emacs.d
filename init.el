e8aa9ea186d36ae77f330c514b72da4de4f99e7b
rice9547 <s5590220001@gmail.com>
2016-07-21 22:18:22 -0700

    debug: emmet

--- init.el
+++ init.el
@@ -37,7 +37,7 @@
 (show-paren-mode t)
 (setq show-paren-style 'parentheses)
 
-(add-to-list 'load-path "~/emacs.d/emmet-mode")
+(add-to-list 'load-path "~/.emacs.d/emmet-mode")
 (require 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
